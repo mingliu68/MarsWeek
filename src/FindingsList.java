72791f76cd6b233973b9954e2c3e2c21e400362d
Ming Liu <mliu@Mings-Mac-mini.local>
2023-02-19 16:27:30 -0500

    FindingsList ok, updated Mars activity selector

--- src/FindingsList.java
+++ src/FindingsList.java
@@ -1,5 +1,76 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
 public class FindingsList {
     public FindingsList() throws InterruptedException {
+
+        Thread.sleep(500);
+
+        System.out.println("Welcome Back!!!");
+
+        List<String> rocks = new ArrayList<String>();
+        System.out.println("\nRock data downloading..... Done.");
+        rocks.add("rock");
+        rocks.add("weird rock");
+        rocks.add("smooth rock");
+        rocks.add("not rock");
+        System.out.println(rocks);
+        System.out.println("\nOne of the item is not an actual rock.  Please remove that from the list.");
+        rocks.remove("not rock");
+        System.out.println(rocks);
+
+        Thread.sleep(1000);
+
+        HashMap<String, String> fossils = new HashMap<>();
+        fossils.put("Bird Fossil", "The fossil has wings implying it was capable of flight");
+        fossils.put("Fish Fossil", "The fossil is vaguely fish shaped implies there was once water" );
+        fossils.put("Tooth Fossil", "The tooth from an unknown fossil");
+        System.out.println("Fossil data downloaded");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("\nWhich of the fossils would you like to learn more about? (Bird, fish, or tooth) ");
+        String selection = scan.nextLine();
+
+        if( selection.equalsIgnoreCase("Bird")) {
+            System.out.println("You've selected " + selection + " fossil. It is " + fossils.get("Bird Fossil") );
+        } else if( selection.equalsIgnoreCase("Fish")) {
+            System.out.println("You've selected " + selection + " fossil. It is " + fossils.get("Fish Fossil") );
+        } else if( selection.equalsIgnoreCase("Tooth")) {
+            System.out.println("You've selected " + selection + " fossil. It is " + fossils.get("Tooth Fossil") );
+        } else {
+            System.out.println("We don't have that in the Fossil data.");
+        }
+
+        Thread.sleep(700);
+
+        HashSet<String> supplies = new HashSet<>();
+
+        supplies.add("Super Energy Shield");
+        supplies.add("Instant Life Boost");
+        supplies.add("Mega Strength");
+
+        System.out.println("\n****___Supplies Before Expedition___****");
+        Iterator itr = supplies.iterator();
+
+        while(itr.hasNext()) {
+            System.out.println("\t" + itr.next());
+        }
+
+        supplies.remove("Mega Strength");
+
+        System.out.println("\n****___Supplies After Expedition___****");
+
+        itr = supplies.iterator();
+
+        while(itr.hasNext()) {
+            System.out.println("\t" + itr.next());
+        }
+
+        scan.close();
 
     }
 }
--- src/Mars.java
+++ src/Mars.java
@@ -30,7 +30,8 @@ public class Mars {
         while (true) {
             System.out.println("\nWhat would you like to do? A or B ?" +
                             "\n\tA: Play the Guessing Game" +
-                            "\n\tB: Explore Mars!");
+                            "\n\tB: Explore Mars!" +
+                            "\n\tC: Check out what we have found on Mars so far!");
         
             activitySelection = scanner.nextLine();
 
@@ -39,14 +40,19 @@ public class Mars {
                 new GuessingGame();
                 break;
             } else if (activitySelection.equalsIgnoreCase("B")) {
-                System.out.println("You selected A, exploring Mars.  Let's go!");
+                System.out.println("You selected B, exploring Mars.  Let's go!");
                 new MarsExpedition();
+                break;
+            } else if (activitySelection.equalsIgnoreCase("C")) {
+                System.out.println("You selected C, checking out what we have found on Mars.  Let's go!");
+                new FindingsList();
                 break;
             } else {
                 System.out.print("Not a valid option. Try Again"); 
             }
         }
 
+        scanner.close();
     }
 
 
